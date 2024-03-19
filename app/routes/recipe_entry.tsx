7f41fc5f439ac4d173bbce9a56175cbf5622ef1b
Manuel Torres <manueljosetorres02@gmail.com>
2024-03-18 23:43:51 -0500

    yes

--- app/routes/recipe_entry.tsx
+++ app/routes/recipe_entry.tsx
@@ -1,19 +1,52 @@
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+
 export default function RecipeEntry() {
+  const [title, setTitle] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [recipe, setRecipe] = useState(null);
+
   const handleSubmit = (e) => {
-    alert("hello");
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    const ingredients = formData.get("ingredients");
+
+    // console.log(title, ingredients);
+    setRecipe({ title, ingredients });
   };
 
   return (
     <div>
       <h1>Recipe Entry</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Recipe Name:</label>
-        <input type="text" id="title" placeholder="Enter recipe name" />
+        <label htmlFor="Recipe Name">Recipe Name:</label>
+        <input
+          id="title"
+          value={title}
+          type="text"
+          name="title"
+          placeholder="Enter recipe name"
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
         <label htmlFor="ingredients">Recipe:</label>
-        <textarea placeholder="Enter your recipe here"></textarea>
+        <textarea
+          id="ingredients"
+          value={ingredients}
+          name="ingredients"
+          placeholder="Enter your recipe here"
+          onChange={(e) => setIngredients(e.target.value)}
+        ></textarea>
         <button type="submit">save </button>
       </form>
+      {recipe && (
+        <div>
+          <h2>{recipe.title}</h2>
+          <p>{recipe.ingredients}</p>
+        </div>
+      )}
     </div>
   );
 }
